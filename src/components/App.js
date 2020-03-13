fb1d7472c1cc6617033765011872ee40ca1149d0
sa-webb <sawebb30@gmail.com>
2020-03-12 19:24:55 -0500

    initial

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Stories from './Stories';
+const App = ({ stories, onArchive }) => (
+  <div className='app'>
+    <Stories stories={stories} onArchive={onArchive} />
+  </div>
+);
+export default App;
---
+++ src/components/Stories.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Stories.css';
+import Story from './Story';
+const COLUMNS = {
+  title: {
+    label: 'Title',
+    width: '40%'
+  },
+  author: {
+    label: 'Author',
+    width: '30%'
+  },
+  comments: {
+    label: 'Comments',
+    width: '10%'
+  },
+  points: {
+    label: 'Points',
+    width: '10%'
+  },
+  archive: {
+    width: '10%'
+  }
+};
+const Stories = ({ stories, onArchive }) => (
+  <div className='stories'>
+    <StoriesHeader columns={COLUMNS} />
+    {(stories || []).map(story => (
+      <Story
+        key={story.objectID}
+        story={story}
+        columns={COLUMNS}
+        onArchive={onArchive}
+      />
+    ))}
+  </div>
+);
+
+const StoriesHeader = ({ columns }) => (
+  <div className='stories-header'>
+    {Object.keys(columns).map(key => (
+      <span key={key} style={{ width: columns[key].width }}>
+        {columns[key].label}
+      </span>
+    ))}
+  </div>
+);
+export default Stories;

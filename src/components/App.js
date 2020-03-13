4f8ac2a64492ce477ca60502852900ece594e8cd
sa-webb <sawebb30@gmail.com>
2020-03-13 11:02:55 -0500

    added search

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import Stories from './Stories';
+import SearchStories from './SearchStories'
+
 const App = ({ stories, onArchive }) => (
   <div className='app'>
+  <div className='interactions'>
+    <SearchStories />
+  </div>
     <Stories />
   </div>
 );

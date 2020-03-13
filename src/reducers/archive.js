5d064010c1c40583458db289fd24fbf4702d6b0f
sa-webb <sawebb30@gmail.com>
2020-03-12 20:43:42 -0500

    archive

--- src/reducers/archive.js
+++ src/reducers/archive.js
@@ -1,7 +1,16 @@
+import { STORY_ARCHIVE } from '../constants/actions';
+
 const INITIAL_STATE = [];
+
+const applyArchiveStory = (state, action) => [...state, action.id];
+
 function archiveReducer(state = INITIAL_STATE, action) {
-  switch(action.type) {
-    default : return state;
+  switch (action.type) {
+    case STORY_ARCHIVE: {
+      return applyArchiveStory(state, action);
+    }
+    default:
+      return state;
   }
 }
 export default archiveReducer;

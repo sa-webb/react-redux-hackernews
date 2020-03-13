b3667bf18aea5a8563d4df1ef52a72f8519fbfdd
sa-webb <sawebb30@gmail.com>
2020-03-13 11:34:14 -0500

    added tests

---
+++ src/selectors/story.test.js
@@ -0,0 +1,17 @@
+import { getReadableStories } from './storySelector';
+describe('story selector', () => {
+  it('retrieves readable stories', () => {
+    const storyState = {
+      error: null,
+      stories: [
+        { objectID: '1', title: 'foo' },
+        { objectID: '2', title: 'bar' },
+      ],
+    };
+    const archiveState = ['1'];
+    const state = { storyState, archiveState }
+    const expectedReadableStories = [{ objectID: '2', title: 'bar' }];
+    const readableStories = getReadableStories(state);
+    expect(readableStories).toEqual(expectedReadableStories);
+  });
+});
